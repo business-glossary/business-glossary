edf6b86cf219a21669371172f64b1ee3b2bd34d9
Alan Tindale <alan@flexiblesoftware.com.au>
2016-11-19 17:50:12 +1000

    Added coverage to the test function

--- manage.py
+++ manage.py
@@ -1,9 +1,17 @@
 import os
 
+COV = None
+
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
+from config import BASE_DIR
 
 app.config.from_object(os.getenv('BG_CONFIG') or 'config.DevelopmentConfig')
 
@@ -13,11 +21,24 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        covdir = os.path.join(BASE_DIR, 'htmlcov')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 if __name__ == '__main__':
     manager.run()

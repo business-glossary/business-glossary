a222f0313aba4e7a892e4fd624bdea6c19596d81
Alan Tindale <alan@flexiblesoftware.com.au>
2017-04-27 20:47:23 +1000

    Rename new term view

--- app/main/routes.py
+++ app/main/routes.py
@@ -197,8 +197,6 @@ def profile():
 @main.route('/term/<int:selected_term>')
 @main.route('/term/<string:selected_term_name>')
 def show_term(selected_term=None, selected_term_name=None):
-    print(">>>>>", selected_term)
-    print(">>>>>", selected_term_name)
 
     if selected_term is None:
         term = Term.query.filter(func.lower(Term.name) == func.lower(selected_term_name)).first()
@@ -210,24 +208,6 @@ def show_term(selected_term=None, selected_term_name=None):
                                                           func.lower(selected_term_name)).first())
     else:
         return render_template('show_term.html',
-                               term=Term.query.filter_by(id=selected_term).first())
-
-
-@main.route('/new_term/<int:selected_term>')
-@main.route('/new_term/<string:selected_term_name>')
-def show_new_term(selected_term=None, selected_term_name=None):
-    '''Present the new term view'''
-
-    if selected_term is None:
-        term = Term.query.filter(func.lower(Term.name) == func.lower(selected_term_name)).first()
-        if not term:
-            return render_template('errors/404.html')
-        else:
-            return render_template('show_new_term.html',
-                                   term=Term.query.filter(func.lower(Term.name) == \
-                                                          func.lower(selected_term_name)).first())
-    else:
-        return render_template('show_new_term.html',
                                term=Term.query.filter_by(id=selected_term).first())
 
 
